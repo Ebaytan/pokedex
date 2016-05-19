46adfe1df2b72c1ce0477c53714e824cec75da16
Ebaytan <ievanbaytan@gmail.com>
2016-05-19 15:59:22 -0400

    Added in pictures of 718 pokemon, working search bar and background Pokemon Center music

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Evan Baytan on 5/17/16.
+//  Copyright © 2016 Ebaytan. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
